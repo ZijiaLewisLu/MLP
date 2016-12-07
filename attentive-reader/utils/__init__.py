5578d4f6d7cc9a51ac77d0c4d7df39287763db71
ZijiaLewisLu <zl1052@nyu.edu>
2016-12-07 11:03:59 +0800

    rm parallel

--- attentive-reader/utils/__init__.py
+++ attentive-reader/utils/__init__.py
@@ -1,8 +1,12 @@
 from tools import pp, array_pad
 from GPU_availability import GPU_availability as GPU
 from data_utils import *
-from parallel import *
+import os
 
-
+def define_gpu(num):
+    gpu_list = GPU()[:num]
+    gpu_str = ','.join(map(str, gpu_list))
+    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_str
+    return gpu_list
 
 
