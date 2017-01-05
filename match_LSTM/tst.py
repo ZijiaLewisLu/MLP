e29a1cb669ba1a72f516f2e6985d9a72c22c22ba
ZijiaLewisLu <zl1052@nyu.edu>
2017-01-05 08:02:03 +0800

    delete

--- match_LSTM/tst.py
+++
@@ -1,9 +0,0 @@
-# import tensorflow as tf
-from match_LSTM import MatchLSTM
-
-batch_size = 1
-vocab_size = 82788
-learning_rate=1e-2
-
-model = MatchLSTM(100, 20, 15, batch_size, vocab_size)
-model.build_model()
