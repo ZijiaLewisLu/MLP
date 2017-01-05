cc29f463282981fded2a8f7bd739865fa12628c7
ZijiaLewisLu <zl1052@nyu.edu>
2017-01-05 08:27:40 +0800

    clean

--- attentive-reader/model/attentive_model.py
+++ attentive-reader/model/attentive_model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.python.ops import rnn_cell
+# from tensorflow.python.ops import rnn_cell
 from base import BaseModel
 
 
@@ -15,29 +15,22 @@ class AttentiveReader(BaseModel):
         embed_d = tf.nn.embedding_lookup(self.emb, self.document, name='embed_d')
         embed_q = tf.nn.embedding_lookup(self.emb, self.query, name='embed_q')
 
-        # embed_d = tf.nn.dropout(embed_d, keep_prob=self.dropout)
-        # embed_q = tf.nn.dropout(embed_q, keep_prob=self.dropout)
+        embed_d = tf.nn.dropout(embed_d, keep_prob=self.dropout)
+        embed_q = tf.nn.dropout(embed_q, keep_prob=self.dropout)
 
         # representation
         with tf.variable_scope("document_represent"):
             # d_t: N, T, Hidden
             d_t, d_final_state = self.rnn( self.size, embed_d, self.d_end, use_bidirection=self.bidirection)
             d_t = tf.concat(2, d_t)
-            d_t = tf.nn.dropout(d_t, keep_prob=self.dropout)
 
         with tf.variable_scope("query_represent"):
             q_t, q_final_state = self.rnn( self.size, embed_q, self.q_end, use_bidirection=self.bidirection)
-
-            if self.bidirection:
-                q_f = tf.unpack(q_t[0], axis=1)
-                q_b = tf.unpack(q_t[-1], axis=1)
-                u = tf.concat(1, [q_f[-1], q_b[0]], name='u')  # N, Hidden*2
-            else:
-                u = tf.unpack(q_t, axis=1)[-1]
-
-            u = tf.nn.dropout(u, keep_prob=self.dropout)
+            u = self.extract_rnn_state( self.bidirection, q_t, self.q_end )
 
 
+        d_t = tf.nn.dropout(d_t, keep_prob=self.dropout)
+        u = tf.nn.dropout(u, keep_prob=self.dropout)
         self.d_t = d_t
         self.u = u
 
@@ -50,16 +43,7 @@ class AttentiveReader(BaseModel):
         W_g = tf.get_variable('W_g', [self.size, self.vocab_size])
         mid = tf.matmul(r, W_rg, name='r_x_W') + \
             tf.matmul(u, W_ug, name='u_x_W')
-        if self.activation == 'relu':
-            g = tf.nn.relu(mid, name='relu_g')
-        elif self.activation == 'tanh':
-            g = tf.tanh(mid, name='g')
-        elif self.activation == 'none':
-            g = mid
-        else:
-            raise ValueError(self.activation)
-
-        g = tf.nn.dropout(g, keep_prob=self.dropout)
+        g = tf.tanh(mid, name='g')
         g = tf.matmul(g, W_g, name='g_x_W')
         self.score = g
 
